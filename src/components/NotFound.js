bbbe140d5cf4dfd57ef6da32be7a8123771a215e
Sinmiloluwa Oloyede <sinmiloluwasunday@yahoo.com>
2020-03-05 15:21:34 +0100

    Add NotFound page

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+	return (
+		<div>
+			<h2 style={{textAlign: 'center'}}>I lead no where yet.</h2>
+		</div>
+	)
+}
+
+export default NotFound
