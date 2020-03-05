8b0289fcd78c60c520eec1e488acc25426b5f5ce
Sinmiloluwa Oloyede <sinmiloluwasunday@yahoo.com>
2020-03-05 15:20:38 +0100

    Add story and user reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import {currentStory, stories} from './storiesReducer';
+import user from './userReducer';
 
 export default combineReducers({
 	currentStory,
-	stories
+	stories,
+	user
 });
