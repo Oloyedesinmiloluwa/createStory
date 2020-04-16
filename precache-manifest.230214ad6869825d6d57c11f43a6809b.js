e15b55a7918f75532d926211f34f653c5c051be7
Sinmiloluwa Oloyede <sinmiloluwasunday@yahoo.com>
2020-04-16 21:53:16 +0100

    Updates

---
+++ precache-manifest.230214ad6869825d6d57c11f43a6809b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4752ea468e0a79e34c9b8f694fbbb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9113ff9ccfdd9edecb7b",
+    "url": "/static/css/main.52434443.chunk.css"
+  },
+  {
+    "revision": "7928e724b23ddd1575f6",
+    "url": "/static/js/2.f033df09.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f033df09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9113ff9ccfdd9edecb7b",
+    "url": "/static/js/main.c408326a.chunk.js"
+  },
+  {
+    "revision": "7cb354b6d3e0ea4432ef",
+    "url": "/static/js/runtime-main.0db7e71d.js"
+  }
+]);
